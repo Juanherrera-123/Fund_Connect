d3eee0fda2923f11f2877e157d5f842abf91d599
Juanherrera-123 <jherreran@unal.edu.co>
2026-01-15 07:59:29 -0500

    Fix missing firebase storage hook imports

--- src/components/PendingReviewView.tsx
+++ src/components/PendingReviewView.tsx
@@ -3,6 +3,7 @@
 import { useMemo } from "react";
 
 import { DEFAULT_FUND_MANAGER_PROFILES, STORAGE_KEYS } from "@/lib/igatesData";
+import { useFirebaseStorage } from "@/lib/useFirebaseStorage";
 import { useLocalStorage } from "@/lib/useLocalStorage";
 import type { FundApplication, Session, UserProfile } from "@/lib/types";
 
